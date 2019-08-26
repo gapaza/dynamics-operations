f628301a19e27a083fbaa67ce699d4b8cc132f49
Gabe Apaza <gapaza@tamu.edu>
2019-08-26 13:21:27 -0500

    starting files

---
+++ coordinate_operations/vector_operations.py
@@ -0,0 +1,8 @@
+import sys, os
+
+
+
+# Vectors are defined by a list.
+# These operations will be meant for vectors in three dimensions.
+# These three dimensional vectors will be modeled by a list of 4 values.
+# The first value is the coordinate system, the last three are the basis multipliers
