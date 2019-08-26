9c363b60c229df6aff056d8ede944e5a500ee0e2
Gabe Apaza <gapaza@tamu.edu>
2019-08-26 14:22:26 -0500

    things

--- coordinate_operations/vector.py
+++ coordinate_operations/vector.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 
 
 
@@ -7,23 +8,64 @@ import sys
 # Vectors are defined by a class
 # These vectors will only have three dimensions
 # These vectors will have a specific coordinate system assigned to them
+# All angle values are in radians
+
+# Cartesian
+# dim1 = x
+# dim2 = y
+# dim3 = z
+
+# Cylindrical
+# dim1 = r
+# dim2 = theta
+# dim3 = z
 
 
 class Vector:
     def __init__(self, system, a1, a2, a3):
-        if(system != "cartesian" and system != "spherical" and system != "cylindrical"):
+        if(system != "cartesian" and system != "cylindrical"):
             raise Exception("invalid coordinate system specified")
-        self.system = system
-        self.x      = a1
-        self.y      = a2
-        self.z      = a3
+        self.system        = system
+        self.dim1          = a1
+        self.dim2          = a2
+        self.dim3          = a3
+        self.valid_systems = ['cartesian', 'cylindrical']
 
     def __str__(self):
-        to_return = "Coordinate System: " + str(self.system) + "\n x = " + str(self.x) + "\n y = " + str(self.y) + "\n z = " + str(self.z)
+        to_return = "Coordinate System: " + str(self.system) + "\nx = " + str(self.dim1) + "\ny = " + str(self.dim2) + "\nz = " + str(self.dim3)
         return to_return
 
     def __add__(self, vec):
         if(self.system != vec.system):
             raise Exception("the basis must be the same when adding vectors")
         if(self.system == "cartesian"):
-            return Vector("cartesian", self.x + vec.x, self.y + vec.y, self.z + vec.z)
+            return Vector("cartesian", self.dim1 + vec.dim1, self.dim2 + vec.dim2, self.dim3 + vec.dim3)
+
+    def change_system(self, new_system):
+        if(self.system == new_system):
+            return True
+        if(new_system not in self.valid_systems):
+            raise Exception('the new coordinate system is not valid')
+
+        if(self.system == 'cartesian'):
+            if(new_system == 'cylindrical'):
+                r     = sqrt(pow(self.dim1,2) + pow(self.dim2,2))
+                theta = atan(self.dim2 / self.dim1)
+                z     = self.dim3
+                self.dim1 = r
+                self.dim2 = theta
+                self.dim3 = z
+                return True
+        if(self.system == 'cylindrical'):
+            if(new_system == 'cartesian'):
+                x = self.dim1 * cos(self.dim2)
+                y = self.dim1 * sin(self.dim2)
+                z = z
+                self.dim1 = x
+                self.dim2 = y
+                self.dim3 = z
+                return True
+
+
+        print("No change occurred")
+        return False
