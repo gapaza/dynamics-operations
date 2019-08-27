b97225e9c0a8982c76147beed73d85518ce6e54d
Gabe Apaza <gapaza@tamu.edu>
2019-08-26 20:25:28 -0500

    things

--- coordinate_operations/basis_operations.py
+++ coordinate_operations/basis_operations.py
@@ -8,9 +8,28 @@ from vector import *
 
 
 
-# Basis_1 is a list of three unit vectors making up basis 1
-# Basis_2 is a list of three unit vectors making up basis 2
-def direction_cosine_matrix(basis_1, basis_2):
-    for vec in (basis_1 + basis_2):
+# Basis_a is a list of three unit vectors making up basis a
+# Basis_b is a list of three unit vectors making up basis b
+def find_direction_cosine_matrix(basis_a, basis_b):
+    for vec in (basis_a + basis_b):
         if(vec.magnitude != 1):
             raise Exception("all the basis vectors must be unit vectors")
+
+    # Recall, basis vectors are unit vectors - so their magnitude is 1
+    # Theta is the angle between the two vectors
+    # b dot a = |b||a|cos(theta)
+    # b dot a = cos(theta)
+    direction_cosine_matrix = []
+    for vec_b in basis_b:
+        row = []
+        for vec_a in basis_a:
+            row.append(vec_b.dot(vec_a))
+        direction_cosine_matrix.append(row)
+    return direction_cosine_matrix
+
+
+
+# Prints a 3x3 array
+def print_direction_cosine_matrix(mtrx):
+    for row in mtrx:
+        print('|' + str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + '|')
--- coordinate_operations/cartesian_operations.py
+++ coordinate_operations/cartesian_operations.py
@@ -5,17 +5,3 @@ import numpy
 import scipy
 
 from vector import *
-
-
-
-
-
-
-
-
-# Basis_1 is a list of three unit vectors making up basis 1
-# Basis_2 is a list of three unit vectors making up basis 2
-def direction_cosine_matrix(basis_1, basis_2):
-    for vec in (basis_1 + basis_2):
-        if(vec.magnitude != 1):
-            raise Exception("all the basis vectors must be unit vectors")
